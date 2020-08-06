c04c89584dd3110b9884af662b17669aff6723f1
Connor ORourke <connororourke101@gmail.com>
2020-08-06 12:44:10 +0100

    function fetchWeatherForecast implemented. all tests passing. part one finished.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const { fetchWeatherData } = require("./functions/weatherFunctions");
+const {
+  fetchWeatherData,
+  getWeatherForecast,
+} = require("./functions/weatherFunctions");
 
 const run = async () => {
   try {
     const weatherData = await fetchWeatherData(60.59329987, -1.44250533);
+    const weatherForecast = getWeatherForecast(weatherData);
+
+    console.log(weatherForecast);
   } catch (err) {
     console.log(err.message);
   }
